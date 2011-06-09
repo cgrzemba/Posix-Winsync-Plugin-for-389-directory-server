f910c23b18eb7c297238fbe6b436e467cfb2da7a
Carsten Grzemba <grzemba@contac-dt.de>
2011-06-09 10:14:20 +0200

    update

--- posix-group-func.c
+++ posix-group-func.c
@@ -1,4 +1,20 @@
-/*
+/** Author: Carsten Grzemba grzemba@contac-dt.de>
+ *
+ * Copyright (C) 2011 contac Datentechnik GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 only
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 	$Id: posix-group-func.c 28 2011-05-13 14:35:29Z grzemba $
 */
 #include "slapi-plugin.h"
